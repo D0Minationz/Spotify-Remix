aeb7b0942a7ecf4de8312a6dbabce80bca6d9cda
LionNub <liondoespk@gmail.com>
2026-04-18 10:53:41 -0400

    Song Done

--- QU-Hackathon-2026Spr/song.js
+++ QU-Hackathon-2026Spr/song.js
@@ -6,10 +6,28 @@ class Song{
         this.picture = pictureURL;
     }
 
-    displayProduct(){
+    constructor(){
+        title = "Unknown Title";
+        artist = "Unknown Artist";
+        picture = ""; //nothing here for now
+    }
+
+    displaySong(){ //"SongObject".displaySong() 
         console.log("Title: " + this.title);
         console.log("Artist: " + this.artist);
         console.log("Picture: " + this.pictureURL);//may not work, not sure how to display picture in console
+    }
+
+    setTitle(newTitle){
+        this.title = newTitle;
+    }
+
+    setArtist(newArtist){
+        this.artist = newArtist;
+    }
+
+    setPicture(newPictureURL){
+        this.picture = newPictureURL;
     }
 
     //const song1 = new Song("Shape of You", "Ed Sheeran", "https://upload.wikimedia.org/wikipedia/en/4/45/Divide_cover.png");
